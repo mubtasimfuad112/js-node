ef2f4a9a878d8a781f16567cd99853e7bb054f05
mubtasimfuad112 <mubtasimfuad079@gmail.com>
2025-08-26 08:56:22 +0000

    datatypes

---
+++ 01.Basics/3_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Data types in JS
+// Numbers => 2 to power 53
+// bigINT
+// String => collection of characters
+// boolean => true/false
+// symbol => unique
+// null => standalone value
+// undefined
+
+// object
+
+let name = "ALex";
+let age = 21;
+let isDriving = true;
+
+console.log(name);
+console.log(age);
+console.log(isDriving);
