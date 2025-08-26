7c3cf4225ecf837dab84f3da10a14e16afaf2596
mubtasimfuad112 <mubtasimfuad079@gmail.com>
2025-08-26 09:04:24 +0000

    datatypes

--- 01.Basics/3_datatypes.js
+++ 01.Basics/3_datatypes.js
@@ -18,3 +18,12 @@ let isDriving = true;
 console.log(name);
 console.log(age);
 console.log(isDriving);
+
+// Using typeof function you can see the type of a variable
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isDriving);
+
+console.log(typeof "Hitesh");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
