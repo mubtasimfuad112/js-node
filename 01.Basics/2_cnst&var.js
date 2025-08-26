2bada56768b829ecd836553a101b75fa1a587ec0
mubtasimfuad112 <mubtasimfuad079@gmail.com>
2025-08-26 08:34:20 +0000

    Variable and Constant

---
+++ 01.Basics/2_cnst&var.js
@@ -0,0 +1,13 @@
+const userID = 1;
+let userEmail = "username@user.com";
+var userPassword = "1234567890";
+userCity = "Dhaka;"
+
+// userID = 2; // Constant can't be changed
+
+console.log(userID);
+
+let Phone;
+
+userPassword = " 0987654321";
+console.table([userID, userEmail, userPassword, userCity, Phone]);
