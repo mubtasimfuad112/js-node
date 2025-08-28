aebe6fd5755bfca873e90c4b05ba0a58568f78df
mubtasimfuad112 <mubtasimfuad079@gmail.com>
2025-08-28 14:54:14 +0000

    comparison operators

---
+++ 01.Basics/6_comparison_operators.js
@@ -0,0 +1,34 @@
+// Simple Comparison Operators
+console.log(2>1); //true
+console.log(2<1); //false
+console.log(2>=1); //true
+console.log(2<=1); //false
+console.log(2!=1); //true
+console.log(1==1); //true
+
+
+// I'm surprised
+console.log("2" > 1);
+console.log("1" == 1);
+// Automatically converts
+
+
+// Lets play with them
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// == and comparison works in different way where comparison compares the null which is same as 0 now.
+
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(typeof (undefined <= 0));
+
+// ===------- Strictly checks datatypes and values
+console.log(1 === 2);
+
+
+
+
+
+
+
