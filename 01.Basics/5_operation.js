da754e06a9c82f5a6a2ea4e779b50e4af223a712
mubtasimfuad112 <mubtasimfuad079@gmail.com>
2025-08-28 14:37:59 +0000

    operations

---
+++ 01.Basics/5_operation.js
@@ -0,0 +1,34 @@
+// ***************** Operation ************************
+let x = 2
+let y = 3
+
+console.log(x+y) // Addition
+console.log(x-y) // Substraction
+console.log(x*y) // Multiplication
+console.log(x/y) // Devision
+console.log(x**y) // To the power of
+console.log(x%y) // Modulo
+
+// Some interesting cases
+console.log(1+2+3) // output is 6
+console.log("1"+2+3); // output is 123
+console.log(1+2+"3"); // but output is 33
+
+// Lets play with some true false
+console.log(+true); // +>1
+console.log(+""); // +>0
+
+// Increment Operator
+let age1 = 18;
+const age2 = age1++;
+console.log(age1,age2);
+
+let num1 = 18;
+const num2 = ++num1;
+console.log(num1,num2);
+
+
+
+
+
+
